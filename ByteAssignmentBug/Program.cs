b9cc902b4b48364bf8e407789fe60d0700a2de0a
Gerard Gunnewijk <gerard.gunnewijk@live.nl>
2021-03-07 00:20:21 +0100

    Add project files.

---
+++ ByteAssignmentBug/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ByteAssignmentBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            Span<byte> span = new byte[1];
+            span[0] = random.NextDouble() < 0.5
+                ? 0x66
+                : 0x6E;
+
+            Console.WriteLine($"The byte in the span is: {span[0]:X2}");
+
+            Console.WriteLine("Done...");
+            Console.ReadLine();
+        }
+    }
+}
